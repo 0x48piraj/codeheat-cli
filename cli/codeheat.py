f70329029ba5cd377c44eaf07ea7e8510d382c10
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-08-18 12:03:02 +0530

    minor: basic command-line iface w/ git auth

--- cli/codeheat.py
+++ cli/codeheat.py
@@ -3,7 +3,26 @@
 from __future__ import print_function, unicode_literals
 from PyInquirer import style_from_dict, Token, prompt, Separator, Validator, ValidationError
 from github import Github
-import sys
+import os, sys, argparse, textwrap, requests
+
+ORG_NAME = 'fossasia'
+REPOS =  [["connfa-android", "open-event-wsgen", "open-event-frontend", "open-event-organizer-android", "open-event-attendee-android", "open-event-ios", "open-event-legacy", "open-event-scraper", "open-event-server", "open-event-orga-iOS", "open-event-theme", "event-collect", "open-event-droidgen", "open-event"],
+         ['pslab-desktop', 'pslab-android', 'pslab-python', 'pslab-firmware', 'pslab.io', 'pslab-webapp', 'pslab-hardware', 'pslab-documentation', 'pslab-case', 'pslab-expeyes', 'pslab-artwork', 'pslab-iOS', 'in.pslab.io', 'jp.pslab.io'],
+         ["meilix.org", "meilix-systemlock", "meilix-artwork", "meilix-generator", "meilix"], "phimpme-android", "susper.com", "badge-magic-android", "badgeyay", "yaydoc"]
+
+banner = textwrap.dedent('''\
+    .===================================================================.
+    ||      codeheat-cli - Command-line tool for winning Codeheat      ||
+    '==================================================================='
+    ''')
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=banner)
+optional = parser._action_groups.pop() # popped opt args
+optional = parser.add_argument_group('Options')
+optional.add_argument("-c", "--cred", dest="creds", action='store_true', help= "for providing Github credentials (username and password)")
+optional.add_argument("-t", "--token", dest="token", action='store_true', help= "for providing Github Developer Token")
+bools = parser.parse_args()
+print(banner)
 
 popauth = [
     {
@@ -18,27 +37,49 @@ popauth = [
     }
 ]
 
-"""
-if not provided on command line args :
-fallback()
-"""
-creds = prompt(popauth)
-print(creds['password'], creds['username'])
+def validate(username, password):
+    r = requests.get('https://api.github.com', auth=(username, password))
+    if r.status_code == 200:
+        return True
+    else:
+        return False
+
+if bools.token == True:
+    print("[*] Token param was selected ...")
+    print("[*] Enter the Github dev token ...")
+    access_token = input("    ----> ")
+    g = Github(access_token) # try, catch
+    print('\n')
+elif bools.creds == True:
+    print("[*] Credentials param was selected ...")
+    print("[*] Enter your Github credentials ...\n")
+    creds = prompt(popauth)
+    gbool = validate(creds['username'], creds['password'])
+    if gbool == True:
+        g = Github(creds['username'], creds['password'])
+        print("[+] Login successful, Github instance correctly initialized.")
+    else:
+        print("[!] Wrong credentials, exiting ...")
+        sys.exit(1)
+    print('\n')
+else:
+    print("No parameter was provided, exiting ...")
+    sys.exit(1)
 
 def handle(answers):
     main, sub = answers['init'], answers['opts']
     if main == 'My status':
-        get_status()
+        get_status(g)
     elif main == 'Top contributors':
         if sub == 'Overall':
-            top_contributors(opt)
+            top_contributors(g, opt)
         else:
-            top_contributors(opt)
+            top_contributors(g, opt)
     elif main == 'Active maintainers':
         if sub == 'Overall':
-            active_maintainers(opt)
+            active_maintainers(g, opt)
         else:
-            active_maintainers(opt)
+            active_maintainers(g, opt)
     elif main == 'Insights':
         get_insights()
 
@@ -62,7 +103,7 @@ questions = [
     {
         'type': 'list',
         'name': 'init',
-        'message': 'Command-line tool for winning Codeheat',
+        'message': 'Enter option : ',
         'choices': [
             'My status',
             'Top contributors',
